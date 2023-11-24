065e985e7c95763e4d4ea1e6eb3606f7e665bc24
Robin Tapiru <robintapiru0894@gmail.com>
2023-11-24 20:06:47 +0400

    initial done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,33 @@ AOS.init({ disable: "mobile" });
 
 const swiper = new Swiper(".sample-slider", {
   loop: true,
-  slidesPerView: 2,
-  spaceBetween: 24,
-  autoplay: {
-    delay: 3000,
+  slidesPerView: 1.6,
+  spaceBetween: 15,
+  navigation: {
+    nextEl: ".next-next",
+    prevEl: ".prev-prev",
   },
-
+  //autoplay: true,
   breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
     1024: {
+      slidesPerView: 4,
+    },
+    1921: {
       slidesPerView: 5,
+      spaceBetween: 24,
     },
   },
+});
+
+$(function () {
+  $("#vidBox").VideoPopUp({
+    backgroundColor: "#17212a",
+    opener: "video1",
+    maxweight: "640",
+    idvideo: "v1",
+  });
 });
